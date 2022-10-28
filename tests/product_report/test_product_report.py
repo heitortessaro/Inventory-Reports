c9a81067579a08d313b96b53bd9f57e31f062f0b
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-10-28 15:32:40 -0300

    test: Create test for Product report

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,30 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    # pass  # Seu teste deve ser escrito aqui
+    id = 1
+    nome_do_produto = "Test product name"
+    nome_da_empresa = "Test company name"
+    data_de_fabricacao = "00-00-0000"
+    data_de_validade = "00-00-0000"
+    numero_de_serie = "123456789"
+    instrucoes_de_armazenamento = "No instructions"
+
+    report = f"""O produto {nome_do_produto}
+        fabricado em {data_de_fabricacao}
+        por {nome_da_empresa} com validade
+        até {data_de_validade}
+        precisa ser armazenado {instrucoes_de_armazenamento}."""
+
+    product = Product(
+        id,
+        nome_do_produto,
+        nome_da_empresa,
+        data_de_fabricacao,
+        data_de_validade,
+        numero_de_serie,
+        instrucoes_de_armazenamento,
+    )
+
+    assert report == product.__repr__
